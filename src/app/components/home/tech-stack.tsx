48cb9a8d1a700a5d8467ffa65acc60c0b2e96497
star-ot <star@oceantech.com>
2023-06-11 21:20:30 -0500

    fix tech stackicons

--- src/app/components/home/tech-stack.tsx
+++ src/app/components/home/tech-stack.tsx
@@ -11,85 +11,85 @@ export default function TechStack() {
                     data-tooltip-id="tooltip-nextjs"
                     data-tooltip-content="Next.js"
                     data-tooltip-place="bottom"
-                    className="sm:py-8 sm:mt-8 hover:animate-pulse text-3xl sm:text-4xl mx-2 text-zinc-500 hover:text-zinc-300 cursor-pointer"
+                    className="hover:animate-pulse text-3xl sm:text-4xl mx-2 text-zinc-500 hover:text-zinc-300 cursor-pointer"
                 />
                 <FaReact
                     data-tooltip-id="tooltip-react"
                     data-tooltip-content="React"
                     data-tooltip-place="bottom"
-                    className="sm:py-8 sm:mt-8 hover:animate-pulse text-3xl sm:text-4xl mx-2 text-zinc-500 hover:text-zinc-300 cursor-pointer"
+                    className="hover:animate-pulse text-3xl sm:text-4xl mx-2 text-zinc-500 hover:text-zinc-300 cursor-pointer"
                 />
                 <FaNodeJs
                     data-tooltip-id="tooltip-nodejs"
                     data-tooltip-content="Node.js"
                     data-tooltip-place="bottom"
-                    className="sm:py-8 sm:mt-8 hover:animate-pulse text-3xl sm:text-4xl mx-2 text-zinc-500 hover:text-zinc-300 cursor-pointer"
+                    className="hover:animate-pulse text-3xl sm:text-4xl mx-2 text-zinc-500 hover:text-zinc-300 cursor-pointer"
                 />
                 <SiPostgresql
                     data-tooltip-id="tooltip-postgresql"
                     data-tooltip-content="PostgreSQL"
                     data-tooltip-place="bottom"
-                    className="sm:py-8 sm:mt-8 hover:animate-pulse text-3xl sm:text-4xl mx-2 text-zinc-500 hover:text-zinc-300 cursor-pointer"
+                    className="hover:animate-pulse text-3xl sm:text-4xl mx-2 text-zinc-500 hover:text-zinc-300 cursor-pointer"
                 />
                 <SiMongodb
                     data-tooltip-id="tooltip-mongodb"
                     data-tooltip-content="MongoDB"
                     data-tooltip-place="bottom"
-                    className="sm:py-8 sm:mt-8 hover:animate-pulse text-3xl sm:text-4xl mx-2 text-zinc-500 hover:text-zinc-300 cursor-pointer"
+                    className="hover:animate-pulse text-3xl sm:text-4xl mx-2 text-zinc-500 hover:text-zinc-300 cursor-pointer"
                 />
                 <SiUpstash
                     data-tooltip-id="tooltip-upstash"
                     data-tooltip-content="Upstash"
                     data-tooltip-place="bottom"
-                    className="sm:py-8 sm:mt-8 hover:animate-pulse text-3xl sm:text-4xl mx-2 text-zinc-500 hover:text-zinc-300 cursor-pointer"
+                    className="hover:animate-pulse text-3xl sm:text-4xl mx-2 text-zinc-500 hover:text-zinc-300 cursor-pointer"
                 />
                 <SiRoblox
                     data-tooltip-id="tooltip-roblox"
                     data-tooltip-content="Roblox"
                     data-tooltip-place="bottom"
-                    className="sm:py-8 sm:mt-8 hover:animate-pulse text-3xl sm:text-4xl ml-2 mr-6 sm:mr-12 text-zinc-500 hover:text-zinc-300 cursor-pointer"
+                    className="hover:animate-pulse text-3xl sm:text-4xl ml-2 mr-6 sm:mr-12 text-zinc-500 hover:text-zinc-300 cursor-pointer"
                 />
                 <SiOpenai
                     data-tooltip-id="tooltip-openai"
                     data-tooltip-content="OpenAI"
                     data-tooltip-place="bottom"
-                    className="sm:py-8 sm:mt-8 hover:animate-pulse text-3xl sm:text-4xl mx-2 text-zinc-500 hover:text-zinc-300 cursor-pointer"
+                    className="hover:animate-pulse text-3xl sm:text-4xl mx-2 text-zinc-500 hover:text-zinc-300 cursor-pointer"
                 />
                 <SiWindows
                     data-tooltip-id="tooltip-windows"
                     data-tooltip-content="Windows"
                     data-tooltip-place="bottom"
-                    className="sm:py-8 sm:mt-8 hover:animate-pulse text-3xl sm:text-4xl mx-2 text-zinc-500 hover:text-zinc-300 cursor-pointer"
+                    className="hover:animate-pulse text-3xl sm:text-4xl mx-2 text-zinc-500 hover:text-zinc-300 cursor-pointer"
                 />
                 <SiUbuntu
                     data-tooltip-id="tooltip-ubuntu"
                     data-tooltip-content="Ubuntu Linux"
                     data-tooltip-place="bottom"
-                    className="sm:py-8 sm:mt-8 hover:animate-pulse text-3xl sm:text-4xl mx-2 text-zinc-500 hover:text-zinc-300 cursor-pointer"
+                    className="hover:animate-pulse text-3xl sm:text-4xl mx-2 text-zinc-500 hover:text-zinc-300 cursor-pointer"
                 />
                 <SiVisualstudiocode
                     data-tooltip-id="tooltip-vscode"
                     data-tooltip-content="Visual Studio Code"
                     data-tooltip-place="bottom"
-                    className="sm:py-8 sm:mt-8 hover:animate-pulse text-3xl sm:text-4xl ml-2 mr-6 sm:mr-12 text-zinc-500 hover:text-zinc-300 cursor-pointer"
+                    className="hover:animate-pulse text-3xl sm:text-4xl ml-2 mr-6 sm:mr-12 text-zinc-500 hover:text-zinc-300 cursor-pointer"
                 />
                 <SiPython
                     data-tooltip-id="tooltip-python"
                     data-tooltip-content="Python"
                     data-tooltip-place="bottom"
-                    className="sm:py-8 sm:mt-8 hover:animate-pulse text-3xl sm:text-4xl mx-2 text-zinc-500 hover:text-zinc-300 cursor-pointer"
+                    className="hover:animate-pulse text-3xl sm:text-4xl mx-2 text-zinc-500 hover:text-zinc-300 cursor-pointer"
                 />
                 <SiTypescript
                     data-tooltip-id="tooltip-typescript"
                     data-tooltip-content="TypeScript"
                     data-tooltip-place="bottom"
-                    className="sm:py-8 sm:mt-8 hover:animate-pulse text-3xl sm:text-4xl mx-2 text-zinc-500 hover:text-zinc-300 cursor-pointer"
+                    className="hover:animate-pulse text-3xl sm:text-4xl mx-2 text-zinc-500 hover:text-zinc-300 cursor-pointer"
                 />
                 <SiLua
                     data-tooltip-id="tooltip-lua"
                     data-tooltip-content="Lua"
                     data-tooltip-place="bottom"
-                    className="sm:py-8 sm:mt-8 hover:animate-pulse text-3xl sm:text-4xl mx-2 text-zinc-500 hover:text-zinc-300 cursor-pointer"
+                    className="hover:animate-pulse text-3xl sm:text-4xl mx-2 text-zinc-500 hover:text-zinc-300 cursor-pointer"
                 />
             </div>
             <Tooltip id="tooltip-nextjs" />
